948426ff723dd2aab92c25c4f75ee3687299c080
Yotam Sela <yotam.sela@mail.huji.ac.il>
2013-03-14 17:00:48 +0200

    first running version, alot of stuff are still missing. menu bar is causeing some king of a bug. usuallt i would commit after the first running version is up, but this is a good git practice for me.

---
+++ TodoListManager/src/il/ac/huji/todolist/TodoListDisplayAdapter.java
@@ -0,0 +1,28 @@
+package il.ac.huji.todolist;
+
+import java.util.List;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class TodoListDisplayAdapter extends ArrayAdapter<String> {
+	
+	public TodoListDisplayAdapter(
+			TodoListManagerActivity activity, List<String> todoList) {
+		super(activity, android.R.layout.activity_list_item, todoList);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		String todoTask = getItem(position);
+		LayoutInflater inflater = LayoutInflater.from(getContext());
+		View view = inflater.inflate(R.layout.row, null);
+		TextView todoTaskName = (TextView)view.findViewById(R.id.todoTaskName);
+		todoTaskName.setText(todoTask);
+		return view;
+	}
+
+}
--- TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,23 +1,63 @@
 package il.ac.huji.todolist;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class TodoListManagerActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todo_list_manager);
-    }
+	private ArrayAdapter<String> adapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_todo_list_manager);
+		//setContentView(R.menu.todo_list_manager);
+
+		List<String> todoList = new ArrayList<String>();
+
+		ListView todoListView = (ListView)findViewById(R.id.lstTodoItems);
+
+		adapter = new TodoListDisplayAdapter(this,todoList);
+		todoListView.setAdapter(adapter);
+
+//		findViewById(R.id.menuItemAdd).setOnClickListener(
+//				new OnClickListener(){
+//					
+//					@Override
+//					public void onClick(View v) {
+//						EditText newTaskTodoView = (EditText)findViewById(R.id.edtNewItem);
+//						String newTaskTodo = newTaskTodoView.getText().toString();
+//						adapter.add(newTaskTodo);
+//					}
+//
+//				});
+
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.todo_list_manager, menu);
-        return true;
-    }
-    
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.todo_list_manager, menu);
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		System.out.println("item.getItemId(): "+ item.getItemId());
+		//switch (item.getItemId())
+		return true;
+	}
+
 }
